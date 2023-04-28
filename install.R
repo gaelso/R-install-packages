b6d38f742bf0fa4c1e0005df1b9f6373105476ad
Gael Sola <gaelsola@hotmail.fr>
2023-04-28 23:47:57 +0700

    Create install.R

---
+++ install.R
@@ -0,0 +1,14 @@
+
+## General data analysis
+install.packages("tidyverse")
+
+## Spatial data analysis
+install.packages(c("sf", "terra", "tidyterra", "dggridR"))
+
+## Python
+install.packages("reticulate")
+
+
+
+
+
