8a887a0aae4dd7fe75c2d585beecdc7472ed4cc9
Gael Sola <gaelsola@hotmail.fr>
2023-04-29 06:24:52 +0700

    Update install.R

--- install.R
+++ install.R
@@ -1,12 +1,44 @@
 
+install_package <- function(.pkg_name) {
+  if (!require(.pkg_name, character.only = TRUE, quietly = TRUE)) install.packages(.pkg_name, dep =TRUE)
+}
+
 ## General data analysis
-install.packages("tidyverse")
+install_package("tidyverse")
+
+##ggplot extensions
+install_package("ggrepel")
+install_package("ggspatial")
+install_package("ggpubr")
+install_package("ggnewscale")
 
 ## Spatial data analysis
-install.packages(c("sf", "terra", "tidyterra", "dggridR"))
+install_package("sf")
+install_package("terra")
+install_package("tidyterra")
+install_package("dggridR")
+install_package("tmap")
 
 ## Python
-install.packages("reticulate")
+install_package("reticulate")
+
+## Shiny apps
+install_package("shiny")
+install_package("shinyjs")
+install_package("shinyWidgets")
+install_package("shinyFiles")
+install_package("DT")
+
+## Communication
+install_package("rmarkdown")
+install_package("blogdwon")
+
+## Custom fonts
+install_package("extrafont")
+install_package("showtext")
+
+
+
 
 
 
